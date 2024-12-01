b317b240f5316a2898e6aca48556ad65d565d7ab
leadav11 <lportocarrerocr@ucvvirtual.edu.pe>
2024-11-30 21:30:13 -0500

    add: Se añadio el componet de questions

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,15 @@
 
 import Header from '../Components/Header'
 import TarjetaCurso from '../Components/TarjetaCurso'
+import Preguntas from '../Components/Preguntas'
+
 
 export default function Home() {
   return (
     <div>
-       <Header/> 
-       <TarjetaCurso/>
+      <Header/> 
+      <TarjetaCurso/>
+      <Preguntas/>
     </div>
   )
 }
