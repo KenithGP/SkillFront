58b983c300972c864659f83923bde25e0d5e43a2
leadav11 <lportocarrerocr@ucvvirtual.edu.pe>
2024-12-02 22:28:38 -0500

    mejora de diseño, titulo

--- src/Components/Preguntas.jsx
+++ src/Components/Preguntas.jsx
@@ -9,106 +9,113 @@ const Preguntas = () => {
 
   const faqs = [
     {
-      question: "¿Cómo puedo registrarme en la plataforma?",
-      answer: "Puedes registrarte creando una cuenta con tu correo electrónico y contraseña en la página de registro.",
+      question: "¿Cómo puedo registrarme en SkillConnect?",
+      answer: "Puedes registrarte de forma gratuita ingresando tu correo electrónico y una contraseña en nuestra página de registro. También puedes vincular tu cuenta de Google para mayor comodidad.",
     },
     {
-      question: "¿Cómo puedo ver mi progreso en un curso?",
-      answer: "El progreso se muestra en la sección 'Mi cuenta', donde puedes ver las lecciones completadas.",
+      question: "¿Cómo puedo acceder a los cursos que compré?",
+      answer: "Una vez que compres un curso, estará disponible en la sección 'Mis Cursos' dentro de tu perfil. Podrás acceder al contenido en cualquier momento desde tu cuenta.",
     },
     {
-      question: "¿Es necesario pagar para acceder a los cursos?",
-      answer: "Algunos cursos son gratuitos y otros son de pago. Los detalles están disponibles en la descripción del curso.",
+      question: "¿Qué beneficios obtengo al completar un curso?",
+      answer: "Al completar un curso, recibirás un certificado avalado por SkillConnect",
     },
     {
-      question: "¿Hay pruebas o evaluaciones en los cursos?",
-      answer: "Sí, algunos cursos incluyen pruebas y ejercicios para evaluar tu progreso.",
+      question: "¿Ofrecen becas en los cursos?",
+      answer: "Sí, contamos con becas parciales.También ofrecemos planes accesibles para estudiantes. Consulta nuestra sección de 'Becas' para más información.",
     },
     {
-      question: "¿Hay clases en vivo o solo material grabado?",
-      answer: "Ofrecemos ambos: material grabado y clases en vivo según el curso.",
+      question: "¿Cómo puedo hacer seguimiento de mi progreso?",
+      answer: "Tu progreso se actualizará automáticamente y estará visible en tu perfil. También recibirás notificaciones de hitos importantes como lecciones completadas y logros obtenidos.",
     },
   ];
 
   return (
   
-    <div className="min-h-screen text-white p-8 flex flex-col items-center">
-    {/* TÍTULO */}
-    <h1 className="text-3xl mb-8">Preguntas frecuentes</h1>
-  
-    {/* Contenedor principal con dos columnas */}
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full max-w-6xl">
-      {/* Columna de cuadro de Preguntas */}
-      <div className="space-y-7">
-        {faqs.map((faq, index) => (
+    <div className="min-h-screen text-yellow-300 p-8 flex flex-col items-center">
+  {/* TÍTULO */}
+  <h1 className="text-4xl font-bold mb-10 relative">
+    Preguntas frecuentes
+  </h1>
+
+  {/* Contenedor principal con dos columnas */}
+  <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full max-w-6xl px-4 sm:px-6">
+    {/* Columna de cuadro de Preguntas */}
+    <div className="space-y-7">
+      {faqs.map((faq, index) => (
+        <div
+          key={index}
+          className="bg-slate-100 text-gray-800 rounded-md shadow-md p-4 transition-transform transform duration-300 ease-in-out hover:scale-105"
+        >
           <div
-            key={index}
-            className="bg-slate-100 text-gray-800 rounded-md shadow-md p-4 transition-transform transform duration-300 ease-in-out hover:scale-105"
+            className="flex justify-between items-center cursor-pointer"
+            onClick={() => toggleAnswer(index)}
           >
-            <div
-              className="flex justify-between items-center cursor-pointer"
-              onClick={() => toggleAnswer(index)}
-            >
-              <p className="text-lg font-medium">{faq.question}</p>
-              <button
-                className="text-xl font-bold text-gray-600 transition-transform transform duration-300 ease-in-out hover:scale-110"
-                aria-label="Toggle Answer"
-              >
-                {activeIndex === index ? (
-                  <span className="text-gray-700">-</span>
-                ) : (
-                  <span className="text-gray-700">+</span>
-                )}
-              </button>
-            </div>
-            {activeIndex === index && (
-              <p className="mt-2 text-sm text-gray-700">{faq.answer}</p>
-            )}
-          </div>
-        ))}
-      </div>
-  
-      {/* Columna del Formulario */}
-      <div className=" text-white">
-        <h2 className="text-2xl mb-4 text-center">¿Tienes alguna otra pregunta?</h2>
-        <p className="text-sm text-gray-200 mb-6 text-center">Nos encantaría resolver tus dudas.</p>
-        <form className="space-y-5">
-          <div>
-            <label className="block text-sm font-normal mb-2">Nombre completo</label>
-            <input
-              type="text"
-              className="w-full border border-gray-300 rounded-md p-2 bg-slate-100 text-gray-800"
-              placeholder="Ingresa tu nombre"
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-normal mb-2">Correo electrónico</label>
-            <input
-              type="email"
-              className="w-full border border-gray-300 rounded-md p-2 bg-slate-100 text-gray-800"
-              placeholder="Ingresa tu correo"
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-normal mb-2">Mensaje</label>
-            <textarea
-              rows="4"
-              className="w-full border border-gray-300 rounded-md p-2 bg-slate-100 text-gray-800 transition duration-300 ease-in-out focus:border-red-500 focus:ring-1 focus:ring-red-500"
-              placeholder="Escribe tu mensaje"
-            ></textarea>
-          </div>
-          <div className="flex justify-center">
+            <p className="text-lg font-medium">{faq.question}</p>
             <button
-              type="submit"
-              className="bg-red-600 text-white py-2 px-5 rounded-md hover:bg-red-700 transition duration-300 ease-in-out"
+              className="text-xl font-bold text-gray-600 transition-transform transform duration-300 ease-in-out hover:scale-110"
+              aria-label="Toggle Answer"
             >
-              Enviar
+              {activeIndex === index ? (
+                <span className="text-gray-700">-</span>
+              ) : (
+                <span className="text-gray-700">+</span>
+              )}
             </button>
           </div>
-        </form>
-      </div>
+          <p
+            className={`mt-2 text-sm text-gray-700 transition-max-height duration-300 ${
+              activeIndex === index ? 'max-h-40' : 'max-h-0 overflow-hidden'
+            }`}
+          >
+            {faq.answer}
+          </p>
+        </div>
+      ))}
+    </div>
+
+    {/* Columna del Formulario */}
+    <div className="text-white mt-10 md:mt-0">
+      <h2 className="text-2xl font-medium text-rose-200 mb-4 text-center">¿Tienes alguna otra pregunta?</h2>
+      <p className="text-sm text-gray-300 mb-6 text-center">Nos encantaría resolver tus dudas.</p>
+      <form className="space-y-5">
+        <div>
+          <label className="block text-sm font-normal mb-2">Nombre completo</label>
+          <input
+            type="text"
+            className="w-full border border-gray-300 rounded-md p-2 bg-slate-100 text-gray-800 transition duration-300 ease-in-out hover:border-red-400 focus:ring-2 focus:ring-red-500"
+            placeholder="Ingresa tu nombre"
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-normal mb-2">Correo electrónico</label>
+          <input
+            type="email"
+            className="w-full border border-gray-300 rounded-md p-2 bg-slate-100 text-gray-800 transition duration-300 ease-in-out hover:border-red-400 focus:ring-2 focus:ring-red-500"
+            placeholder="Ingresa tu correo"
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-normal mb-2">Mensaje</label>
+          <textarea
+            rows="4"
+            className="w-full border border-gray-300 rounded-md p-2 bg-slate-100 text-gray-800 transition duration-300 ease-in-out focus:border-red-500 focus:ring-1 focus:ring-red-500"
+            placeholder="Escribe tu mensaje"
+          ></textarea>
+        </div>
+        <div className="flex justify-center">
+          <button
+            type="submit"
+            className="bg-red-600 text-white py-2 px-5 rounded-md hover:bg-red-700 focus:ring-2 focus:ring-red-400 focus:outline-none transition duration-300 ease-in-out"
+          >
+            Enviar
+          </button>
+        </div>
+      </form>
     </div>
   </div>
+</div>
+
   
   
   
