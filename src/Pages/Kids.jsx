e5edece9b1a25dcf80a99e015a29003fa6a1942e
leadav11 <lportocarrerocr@ucvvirtual.edu.pe>
2024-12-02 22:29:16 -0500

    mejora de diseño, preguntas

---
+++ src/Pages/Kids.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from '../Components/Header'
+import TarjetaCurso from '../Components/TarjetaCurso'
+import TarjetPlanes from '../Components/TarjetPlanes'
+import Preguntas from '../Components/Preguntas'
+import Footer from '../Components/Footer'
+import Slider from '../Components/Slider'
+
+
+export default function Kids() {
+return (
+    <div  className="bg-gradient-to-b from-[#000000]/100  to-[#EA6558]/100 max-screen">
+    <Header/>
+    <Slider variant='default'/>        
+    <TarjetPlanes/>      
+    <Preguntas/>
+    <TarjetaCurso/>
+    <Footer/>
+    </div>
+)
+}
