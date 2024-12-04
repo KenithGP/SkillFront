b9cd8cc8e3b6f2ffc70ea9b0e98b054badf0e40f
KenithGP <kenithguanilo@gmail.com>
2024-12-04 00:58:31 -0500

    feat : visual password

--- src/Components/RegisterAccount.jsx
+++ src/Components/RegisterAccount.jsx
@@ -1,11 +1,14 @@
+
 import Logo from "../assets/Icons/Logo.svg";
 
 export default function RegisterAccount() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
   return (
     <div className="min-h-screen bg-white flex flex-col justify-center items-center py-12 px-6 lg:px-8">
       <div className="max-w-xl w-full space-y-8 border border-gray-300 rounded-lg p-6 shadow-2xl">
         <div className="text-center">
-          {/* Logo */}
           <div className="flex justify-center mt-5 items-center">
             <img className="h-16 w-30" src={Logo} alt="Logo" />
           </div>
@@ -39,7 +42,6 @@ export default function RegisterAccount() {
         </div>
 
         <form className="mt-8 space-y-6" action="#">
-          {/* Fila para Nombre y Apellido */}
           <div className="flex space-x-4">
             <div className="w-1/2">
               <label
@@ -75,7 +77,6 @@ export default function RegisterAccount() {
             </div>
           </div>
 
-          {/* Correo Electrónico */}
           <div>
             <label
               htmlFor="email-address"
@@ -93,7 +94,6 @@ export default function RegisterAccount() {
             />
           </div>
 
-          {/* Fecha de Nacimiento */}
           <div>
             <label
               htmlFor="birthdate"
@@ -110,9 +110,8 @@ export default function RegisterAccount() {
             />
           </div>
 
-          {/* Contraseña y Confirmar Contraseña */}
           <div className="flex space-x-4">
-            <div className="w-1/2">
+            <div className="w-1/2 relative">
               <label
                 htmlFor="password"
                 className="block text-sm font-medium text-gray-700"
@@ -122,13 +121,41 @@ export default function RegisterAccount() {
               <input
                 id="password"
                 name="password"
-                type="password"
+                type={showPassword ? "text" : "password"}
                 required
                 className="mt-1 block w-full rounded-md px-2 h-10 shadow-md sm:text-sm border border-gray-300"
                 placeholder="Escribe tu contraseña"
               />
+              <span
+                className="absolute top-2/3 right-3 transform -translate-y-1/2 cursor-pointer"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-gray-500"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  {showPassword ? (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0zM2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-.965 3.057-4.755 6-9.542 6-4.477 0-8.268-2.943-9.542-6z"
+                    />
+                  ) : (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M3 3l18 18M9.257 9.257a3 3 0 004.486 4.486M14.743 14.743a3 3 0 01-4.486-4.486M15.536 8.464c2.598 2.598 3.104 5.678 1.344 7.438M8.464 15.536c-2.598-2.598-3.104-5.678-1.344-7.438"
+                    />
+                  )}
+                </svg>
+              </span>
             </div>
-            <div className="w-1/2">
+            <div className="w-1/2 relative">
               <label
                 htmlFor="confirm-password"
                 className="block text-sm font-medium text-gray-700"
@@ -138,11 +165,39 @@ export default function RegisterAccount() {
               <input
                 id="confirm-password"
                 name="confirm-password"
-                type="password"
+                type={showConfirmPassword ? "text" : "password"}
                 required
                 className="mt-1 block w-full rounded-md px-2 h-10 shadow-md sm:text-sm border border-gray-300"
                 placeholder="Confirma tu contraseña"
               />
+              <span
+                className="absolute top-2/3 right-3 transform -translate-y-1/2 cursor-pointer"
+                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-gray-500"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  {showConfirmPassword ? (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0zM2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-.965 3.057-4.755 6-9.542 6-4.477 0-8.268-2.943-9.542-6z"
+                    />
+                  ) : (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M3 3l18 18M9.257 9.257a3 3 0 004.486 4.486M14.743 14.743a3 3 0 01-4.486-4.486M15.536 8.464c2.598 2.598 3.104 5.678 1.344 7.438M8.464 15.536c-2.598-2.598-3.104-5.678-1.344-7.438"
+                    />
+                  )}
+                </svg>
+              </span>
             </div>
           </div>
 
