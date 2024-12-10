8c9400d794d16f25fefd54bbd751a433f97a2be7
zentoo31 <zentoo31@gmail.com>
2024-12-10 16:40:21 -0500

    add service for update info user

--- src/services/user.info.service.ts
+++ src/services/user.info.service.ts
@@ -35,4 +35,22 @@ export class UserInfoService{
         }
     }
 
+    async updateUserInfo(first_name:string, last_name: string, email:string, birthdate:string, username:string){
+        try {
+            const response = await fetch(`${this.baseURL}/update-info`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                credentials: 'include',
+                body: JSON.stringify({first_name, last_name, email, birthdate, username})
+            });
+            const user = await response.json();
+            return user;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+
 }
